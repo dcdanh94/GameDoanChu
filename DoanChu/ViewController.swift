c1cb7c6a8013296253f8ca2c96d0e1f6c054dd47
dcdanh94 <dcdanh94@gmail.com>
2016-12-03 21:58:12 +0700

    first commit

---
+++ DoanChu/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  DoanChu
+//
+//  Created by Cong Danh on 12/3/16.
+//  Copyright © 2016 Cong Danh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+   //     self.Img1.image = UIImage(named: "english-enimals")
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
